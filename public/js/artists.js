6e4cd7b6563557af8d691216fdb123868d4ded95
Janik Hotz <snangles.void@gmail.com>
2018-10-08 16:16:48 +0200

    artist preview

--- public/js/artists.js
+++ public/js/artists.js
@@ -1,20 +1,23 @@
 function artistLinkMouseover(event) {
-    let src = $(event).attr('thumbnail-src')
-    if (src) {
-        let $img = $('.picture-container img')
-        let op = $img.css('opacity')
-        if (op == 0) {
-            $img.css('opacity', 1)
-            $img.attr('src', src)
-        }
-        else {
-            $img.css('opacity', 0)
-            setTimeout(() => {
-                $img.css('opacity', 1)
-                $img.attr('src', src)
-            }, 10)
-        } 
-    }
+    // let src = $(event).attr('thumbnail-src')
+    let artist = $(event).attr('artist')
+    $('.preview').css('display', 'none')
+    $(`.preview[artist="${artist}"]`).css('display', 'block')
+    // if (src) {
+    //     let $img = $('.picture-container img')
+    //     let op = $img.css('opacity')
+    //     if (op == 0) {
+    //         $img.css('opacity', 1)
+    //         $img.attr('src', src)
+    //     }
+    //     else {
+    //         $img.css('opacity', 0)
+    //         setTimeout(() => {
+    //             $img.css('opacity', 1)
+    //             $img.attr('src', src)
+    //         }, 10)
+    //     } 
+    // }
 }
 function artistLinkMouseout(event){
     let $img = $('.picture-container img')
