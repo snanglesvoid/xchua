ab1560d267d1de12534406dfa82c9bfddf085d5b
Janik Hotz <snangles.void@gmail.com>
2018-11-15 13:46:44 +0100

    api 2

---
+++ routes/api/artwork.js
@@ -0,0 +1,53 @@
+const keystone = require('keystone')
+const Artwork = keystone.list('Artwork').model
+
+exports = module.exports = {
+    get: (req, res) => {
+        Artwork.findOne(req.body)
+            .exec((err, doc) => {
+                if (err) {
+                    return res.status(500).send(err)
+                }
+                res.json(doc)
+            })
+    },
+    post: (req, res) => {
+        let artwork = req.body.artwork
+        let Artwork
+        Artwork.findOne(artwork)
+            .exec((err, doc) => {
+                if (err) {
+                    return res.status(500).send(err)
+                }
+                if (!doc) {
+                    let w = new Artwork(artwork)
+                    w.save(err => {
+                        if (err) {
+                            return res.status(500).send(err)
+                        }
+                        res.json({
+                            status: 'success',
+                            msg: 'inserted document into database!'
+                        })
+                    })
+                }
+                else {
+                    doc.set(artwork)
+                    doc.save(err => {
+                        if (err) {
+                            return res.status(500).send(err)
+                        }
+                        res.json({
+                            status: 'success',
+                            msg: 'update document in database'
+                        })
+                    })
+                }
+            })
+    },
+    delete: (req, res) => {
+        res.status(500).send({
+            msg: 'this feature is not enabled yet.'
+        })
+    }
+}
