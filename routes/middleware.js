5b28b8d829372187c1af1ab5003aa545d85d9fd5
Janik Hotz <snangles.void@gmail.com>
2019-05-31 15:26:55 +0200

    api update

--- routes/middleware.js
+++ routes/middleware.js
@@ -41,6 +41,7 @@ exports.cors = function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next()
 }
 
 exports.initLocals = function (req, res, next) {
