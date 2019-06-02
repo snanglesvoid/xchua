2d44da4f698a0d449e90463914769bc28f68d7ff
Janik Hotz <snangles.void@gmail.com>
2019-06-02 17:03:05 +0200

    artworks api

--- routes/api/artworks.js
+++ routes/api/artworks.js
@@ -2,17 +2,19 @@ const keystone = require('keystone')
 
 exports = module.exports = {
     get: (req, res) => {
-        const query = keystone.list('Artwork').model.find()
+        const query = keystone.list('Artwork').model.find({
+            artist : { $ne : null }
+        })
 
         if (req.params.artistId) {
             query.where({ artist: req.params.artistId })
         }
        
         query.exec((err, docs) => {
-                if (err) {
-                    return res.status(500).send(err)
-                }
-                return res.json(docs)
-            })
+            if (err) {
+                return res.status(500).send(err)
+            }
+            return res.json(docs)
+        })
     }
 }
