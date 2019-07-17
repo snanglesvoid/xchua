466661097f56edd528e6c0cb45e1f70f99074730
Janik Hotz <snangles.void@gmail.com>
2019-07-17 13:01:28 +0200

    admin api

--- routes/admin/frontPageImages.js
+++ routes/admin/frontPageImages.js
@@ -35,7 +35,7 @@ exports = module.exports = {
 		let id = req.params.id
 		keystone
 			.list('FrontPageImage')
-			.model.deleteOne({ _id: id })
+			.model.findByIdAndRemove(id)
 			.exec((err, _) => {
 				if (err) return res.status(500).send(err)
 				res.json(_)
