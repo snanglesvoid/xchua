982c10e34b905c34e0eb7121e44f223b08cf2b99
Janik Hotz <snangles.void@gmail.com>
2019-08-19 15:45:23 +0200

    artwork state field added

--- models/ShelfRow.js
+++ models/ShelfRow.js
@@ -2,7 +2,7 @@ const keystone = require('keystone')
 const Types = keystone.Field.Types
 
 const ShelfRow = new keystone.List('ShefRow', {
-	map: { name: index },
+	map: { name: 'index' },
 })
 
 ShelfRow.add({
