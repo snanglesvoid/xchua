0928b33241331c6f174cb3b91d3cd14a02ecbc4c
Janik Hotz <snangles.void@gmail.com>
2019-08-22 15:25:21 +0200

    lib-article api update

--- routes/api/lib-articles.js
+++ routes/api/lib-articles.js
@@ -1,13 +1,23 @@
 const keystone = require('keystone')
 
-exports = module.exports = (req, res) => {
-	keystone
-		.list('LibArticle')
-		.model.find({ state: 'published' })
-		.exec((err, docs) => {
-			if (err) {
-				return res.status(500).send(err)
-			}
-			res.json(docs)
-		})
+exports = module.exports = async (req, res) => {
+	try {
+		let rows = await keystone.list('ShelfRow').model.find()
+		let items = await keystone
+			.list('LibArticle')
+			.model.find({ state: 'published' })
+		let result = rows.map(x => items.filter(y => y.row == x))
+		res.json(result)
+	} catch (error) {
+		res.status(500).send(error)
+	}
+	// keystone
+	// 	.list('LibArticle')
+	// 	.model.find({ state: 'published' })
+	// 	.exec((err, docs) => {
+	// 		if (err) {
+	// 			return res.status(500).send(err)
+	// 		}
+	// 		res.json(docs)
+	// 	})
 }
