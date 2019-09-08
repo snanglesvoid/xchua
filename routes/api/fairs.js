10b1f1eaf7d6d684d18394b1a7c317bad947daa3
Janik Hotz <snangles.void@gmail.com>
2019-09-08 14:14:14 +0200

    fairs api sorting

--- routes/api/fairs.js
+++ routes/api/fairs.js
@@ -1,12 +1,15 @@
 const keystone = require('keystone')
 
 exports = module.exports = (req, res) => {
-    keystone.list('Fair').model.find({ state: 'published' })
-        .populate('artists')
-        .exec((err, docs) => {
-            if (err) {
-                return res.status(500).send(err)
-            }
-            res.json(docs)
-        })
+	keystone
+		.list('Fair')
+		.model.find({ state: 'published' })
+		.populate('artists')
+		.sort('-date.start')
+		.exec((err, docs) => {
+			if (err) {
+				return res.status(500).send(err)
+			}
+			res.json(docs)
+		})
 }
